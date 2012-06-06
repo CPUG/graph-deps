ea39c885f334f38ea9d2431f1e75d383d427c71b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-06 21:52:32 +0200

    Solution + some extra thingies.

--- src/dojo_coding_1/core.clj
+++ src/dojo_coding_1/core.clj
@@ -1,8 +1,17 @@
 (ns dojo-coding-1.core
   (:use [midje.sweet]))
 
-(defn graph-lines ""
-  [])
+(defn- graph-lines "Given a graph, create the graphviz dot lines in a seq. Add the nodes then the relations between them."
+  [g]
+  (let [ks (keys g)]
+    (concat
+     ["digraph fn {"]
+     (map #(str "\"" % "\";") ks)
+     (mapcat
+      (fn [k]
+        (map #(str "\"" k "\" -> \"" % "\";") (g k)))
+      ks)
+     ["}"])))
 
 (fact
   (graph-lines {"node1" #{"node2"}
@@ -16,4 +25,24 @@
                                    "\"node2\" -> \"node1\";"
                                    "\"node2\" -> \"node3\";"
                                    "}"] :in-any-order))
+
+m
+(keys m)
+(vals m)
+
+(def m {:key0 [:key1]
+        :key1 [:key2 :key3]
+        :key2 []
+        :key3 []})
+
+(mapcat (fn [n]
+          (for [v (m n)]
+            (str n " -> " v)))
+        (keys m))
+
+(for [n (keys m)
+      v (m n)]
+  (str n " -> " v))
+
+
 
