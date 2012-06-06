eaa3984163ab459614a6fdda4c76d53c8acbae48
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-06 19:37:17 +0200

    Fact to begin the work.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject dojo-coding-1 "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Graph your files"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [midje "1.3.1"]])
--- src/dojo_coding_1/core.clj
+++ src/dojo_coding_1/core.clj
@@ -1,1 +1,18 @@
-(ns dojo-coding-1.core)
+(ns dojo-coding-1.core
+  (:use [midje-sweet]))
+
+(defn graph-lines ""
+  [])
+
+(fact
+  (graph-lines {"node1" #{"node2"}
+                "node2" #{"node1" "node3"}
+                "node3" #{}}) => (just
+                                  ["digraph fn {"
+                                   "\"node1\";"
+                                   "\"node2\";"
+                                   "\"node3\";"
+                                   "\"node1\" -> \"node2\";"
+                                   "\"node2\" -> \"node1\";"
+                                   "\"node2\" -> \"node3\";"
+                                   "}"] :in-any-order))
